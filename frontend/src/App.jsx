7ab0e46f65216b7b160b1e79ace5e086f2f37850
mn842001 <148740422+mn842001@users.noreply.github.com>
2025-11-03 15:16:13 -0600

    Quick commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,4 @@
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
-} from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route,Navigate,} from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
