65a064f4837e1f3eeac01512d8b496173e45dab8
Jace Barefield <jdb152@students.uwf.edu>
2025-11-06 12:49:40 -0600

    salting functionality in seed

--- backend/seed.js
+++ backend/seed.js
@@ -1,6 +1,7 @@
 //seed file for practitioner role
 import Practitioner from './models/Practitioner.js';
 import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
 
 
 const seedPractitioners = async () => {
@@ -15,21 +16,23 @@ const seedPractitioners = async () => {
             {
                 Email: "janesmith@example.com",
                 Name: "Jane Smith",
-                Password: "password123",
+                Password: "password1234",
             },
             {
                 Email: "alicejohnson@example.com",
                 Name: "Alice Johnson",
-                Password: "password123",
-            },
+                Password: "password12345",
+            }
         ];
-
+        // Hash passwords before inserting
+        for (let practitioner of practitioners) {
+            const salt = await bcrypt.genSalt(10);
+            practitioner.Password = await bcrypt.hash(practitioner.Password, salt);
+        }
         await Practitioner.insertMany(practitioners);
         console.log("Practitioners seeded successfully");
     } catch (error) {
         console.error("Error seeding practitioners:", error);
-    } finally {
-        mongoose.connection.close();
     }
 };
 
