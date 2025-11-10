2de4bd54a98afde971c7a68767bdced83079f6b9
mn842001 <148740422+mn842001@users.noreply.github.com>
2025-11-10 15:31:53 -0600

    jace merge

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,11 +3,22 @@ import mongoose from 'mongoose';
 export const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log('MongoDB connected ${conn.connection.host}')
+    console.log(`MongoDB connected ${conn.connection.host}`);
+
 } catch(err) {
     console.log(err);
     process.exit(1);
-  }
-};
 
+}
+
+}
+
+export const mongoAdminConnection = async () => {
+    try {
+        const adminConn = await mongoose.connect(process.env.MONGO_ADMIN_URI);
+        console.log(`MongoDB Admin connected ${adminConn.connection.host}`);
+    } catch(err) {
+        console.log(err);
+        process.exit(1);
+    }
 }
